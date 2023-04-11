2b115c8db08d0e73db73e0b639c38222989af196
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-11 13:14:42 -0500

    unsuccessful test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [heading, setHeading] = useState("Magnificent Monkeys");
+
+  const clickHandler = () => {
+    setHeading("Radical Rhinos");
+  };
+
   return (
-    <div className="App">
-      <h1>Our First Test</h1>
-    </div>
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
   );
-}
+};
 
 export default App;
