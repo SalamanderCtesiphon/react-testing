b8407c2e5c4de1fd40e5e4dfbba468ba65c153b3
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-11 13:09:52 -0500

    ran my first test yay?????

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe("App component", () => {
+  it("renders correct heading", () => {
+    render(<App />);
+    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
+  });
 });
